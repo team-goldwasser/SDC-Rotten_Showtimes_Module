e52e80f3dc52246b88bb2e11d287fd3f3083070e
Chen Wilson <chenlwilson@gmail.com>
2019-02-02 10:29:35 -0800

    Connected Sequelize to PSQL 'rottentomatoes' db

--- showtimes/server.js
+++ showtimes/server.js
@@ -5,3 +5,20 @@ const port = 9002;
 
 app.listen(port);
 
+const sequelize = new Sequelize({
+  database: 'rottentomatoes',
+  username: 'Chen',
+  password: null,
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  port: 5432,
+  define: { timestamps: false }
+})
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.')
+  })
+  .catch(err => {
+    console.log('Unable to connect to the database: ', err);
+  })
