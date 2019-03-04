f17a11f02918a8625cc6e7eced60d6105dd74ef7
Chen Wilson <chenlwilson@gmail.com>
2019-03-04 21:37:28 +1000

    Added err handling for na movies & env var

--- helpers/getInfo.js
+++ helpers/getInfo.js
@@ -5,12 +5,23 @@ const getInfo = (zip, callback) => {
   const titleUrl = window.location.pathname.split('/m/')[1].replace('/', '');
   console.log(titleUrl);
   $.ajax({
-    url: `/showtime/${titleUrl}/${zip}`,
+    url: `http://localhost:9002/showtime/${titleUrl}/${zip}`,
     type: 'GET',
     contentType: 'application/json',
   })
     .done((data) => {
       console.log(data);
+      if (data.showtimes.length === 0) {
+        const showtimes = [
+          { id: 2989, start_time: '11:45', seat: 'recliner' },
+          { id: 2991, start_time: '16:45', seat: 'recliner' },
+          { id: 2992, start_time: '17:15', seat: '3D' },
+          { id: 2992, start_time: '19:45', seat: 'standard' },
+          { id: 2993, start_time: '21:30', seat: '3D' },
+          { id: 2994, start_time: '22:15', seat: 'recliner' },
+        ];
+        data.showtimes = showtimes;
+      }
       callback(data);
     })
     .fail(() => {
@@ -20,10 +31,10 @@ const getInfo = (zip, callback) => {
         week_day: new Date().getDay(),
         showtimes: [
           { id: 2989, start_time: '11:45', seat: 'recliner' },
-          { id: 2990, start_time: '13:15', seat: 'standard' },
           { id: 2991, start_time: '16:45', seat: 'recliner' },
+          { id: 2992, start_time: '17:15', seat: '3D' },
           { id: 2992, start_time: '19:45', seat: 'standard' },
-          { id: 2993, start_time: '21:30', seat: 'recliner' },
+          { id: 2993, start_time: '21:30', seat: '3D' },
           { id: 2994, start_time: '22:15', seat: 'recliner' },
         ],
         theater_id: 2,
