afa02da398c2f55d237faf2f27ce2f9d8a755e95
EC2 Default User <ec2-user@ip-172-31-31-172.us-west-2.compute.internal>
2019-04-25 00:53:01 +0000

    added loader.io verification file

--- seeders/20190202201451-demo-movie.js
+++ seeders/20190202201451-demo-movie.js
@@ -1,14 +1,7 @@
 const generateMovie = require('../db/utils/genMovie.js');
 
 module.exports = {
-<<<<<<< seeding_ec2_postgres
   up: async (queryInterface, Sequelize) => await queryInterface.bulkInsert('movies', generateMovie(), {}),
 
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete('movies', null, {}),
 };
-=======
-  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('movies', generateMovie(), {}),
-
-  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('movies', null, {}),
-};
->>>>>>> master
