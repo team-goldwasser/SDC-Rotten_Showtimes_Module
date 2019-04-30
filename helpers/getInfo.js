55f616020a30b70f029096f64d7efb012614225d
EC2 Default User <ec2-user@ip-172-31-27-28.us-west-2.compute.internal>
2019-04-30 06:52:29 +0000

    added ip address to this instance2

--- helpers/getInfo.js
+++ helpers/getInfo.js
@@ -32,7 +32,7 @@ const getInfo = (zip, callback) => {
   // console.log('inside getInfo B:', zip);
 
   $.ajax({
-      url: `http://35.164.241.88:9002/showtime/${titleUrl}/${zip}`,
+      url: `http://34.211.254.236:9002/showtime/${titleUrl}/${zip}`,
      // url: `http://localhost:9002/showtime/${titleUrl}/${zip}`,
       type: 'GET',
       contentType: 'application/json',
